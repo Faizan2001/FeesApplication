c8d99fe669cda7f8d335bdf0ed2741aedbae67a0
Faizan2001 <faizan.syed2001@gmail.com>
2021-10-17 00:47:30 +0600

    Commit - 1

---
+++ app/src/main/java/com/example/feesapplication/data/Student.kt
@@ -0,0 +1,15 @@
+package com.example.feesapplication.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "students")
+data class Student(
+    @PrimaryKey(autoGenerate = true) val id:Int = 0,
+    @ColumnInfo(name = "name") val studentName: String,
+    @ColumnInfo(name = "number") val studentNumber: Int,
+    @ColumnInfo(name = "email") val studentEmail: String,
+    @ColumnInfo(name = "status") val feesStatus: Boolean,
+    @ColumnInfo(name = "fees") val feesAmount: Int)
