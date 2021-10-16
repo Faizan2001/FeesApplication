7d80f9118e90d5a8f1ffbf101acf4aa48cd2df5a
Faizan2001 <faizan.syed2001@gmail.com>
2021-10-17 01:01:58 +0600

    Commit - 1

--- app/src/main/java/com/example/feesapplication/StudentApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.feesapplication
-
-import android.app.Application
-import com.example.feesapplication.data.StudentRoomDatabase
-
-class StudentApplication : Application(){
-    val database: StudentRoomDatabase by lazy {StudentRoomDatabase.getDatabase( this)}
-}
